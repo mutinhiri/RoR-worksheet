c67d8efbb16eac0f15f5ffae72026fb570ad7113
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-21 21:16:55 +0200

    Add changes

--- db/migrate/20220421190939_create_articles.rb
+++ db/migrate/20220421190939_create_articles.rb
@@ -1,8 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.description :description
 
       t.timestamps
     end
---
+++ db/migrate/20220421191600_add_length_to_articles.rb
@@ -0,0 +1,4 @@
+class AddLengthToArticles < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
