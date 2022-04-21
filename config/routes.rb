cf5ae870276720fa56cf2935fb23e72c24c171a6
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-21 20:34:24 +0200

    Add routing path

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  get 'about', to: 'pages#about'
 end
