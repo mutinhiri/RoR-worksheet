21db107f99a11b6ec82d6fd8bad5f706edc3ba60
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 13:25:43 +0200

    Asdd new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,6 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article].inspect
-        @article = Article.new
+        @article = Article.new(article_params)
     end
 end 
