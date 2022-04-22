e514e8b670bc74a6b30eb79e500a473f75caf33f
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 13:31:54 +0200

    Add new description

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,6 @@ class ArticlesController < ApplicationController
 
     private
         def article_params
-            params.require(:article).permit(:title)
+            params.require(:article).permit(:title, :description)
         end
 end 
