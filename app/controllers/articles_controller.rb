248c86c1b5eacb9aa7b264807519fcdfb40a65c7
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 14:14:14 +0200

    Add new app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,6 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article successfully creatyesd "
