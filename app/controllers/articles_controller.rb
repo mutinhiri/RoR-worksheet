08e761e677412a3ebfad14d557571a0cf196b21b
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 13:59:57 +0200

    Add new method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,9 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article].inspect
-        @article = Article.new(article_params)
-        @article.save
-        redirect_to article_path(@article)
+        # @article = Article.new(article_params)
+        # @article.save
+        # redirect_to article_path(@article)
     end
 
     private
