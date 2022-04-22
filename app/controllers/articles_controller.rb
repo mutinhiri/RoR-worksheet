91bb028ba6e4cfc2232188cdf0aaae3958506902
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 14:06:47 +0200

    Add new article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         # @article = Article.new(article_params)
-        # @article.save
+        @article.save
         # redirect_to article_path(@article)
 
     end
