8cfe2f2429ffd81d6b6235c927de0ba506be3b25
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 14:57:13 +0200

    Fix bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
     private
         def article_params
             params.require(:article).permit(:title, :description)
