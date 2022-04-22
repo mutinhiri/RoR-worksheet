9a47e2c5a5d17dc1fd4112ea3d34816260757131
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-22 14:13:59 +0200

    Create and initiate new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article].inspect
-        # @article = Article.new(article_params)
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article successfully creatyesd "
             redirect_to article_path(@article)
