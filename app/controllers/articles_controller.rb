7794905819e445e29b44b1a1b5a5dd32acfb15d7
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-23 21:06:31 +0200

    Afd pvt method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,8 +47,9 @@ class ArticlesController < ApplicationController
     private
 
         def set_article
+            @article = Article.find(params[:id])
         end
-        
+
         def article_params
             params.require(:article).permit(:title, :description)
         end
