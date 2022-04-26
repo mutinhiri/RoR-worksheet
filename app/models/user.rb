a04b2c216d1186c73408bafab65cb4bef3371e19
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-26 21:48:11 +0200

    Add regular expesiions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
