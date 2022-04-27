1809f1410e5967c40ad499cc53248d310e0dab59
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 09:18:40 +0200

    Add changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        debug
+        
         @article = Article.new(article_params)
         # @article.user = User.first
         if @article.save
