63ba56c5e664563486a87fb72fe4479f50762295
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 09:16:39 +0200

    Check bugs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     def create
         debugger 
         @article = Article.new(article_params)
-        @article.user = User.first
+        # @article.user = User.first
         if @article.save
             flash[:success] = "Article successfully creatyesd "
             redirect_to article_path(@article)
