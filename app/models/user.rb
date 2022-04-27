9da71b2b8e1d98430353b42128b65e3c197e5bad
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 08:57:01 +0200

    Before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :articles
+    before_save { }
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
