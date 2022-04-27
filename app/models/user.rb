d5fb20ef7c4a29b7081fb0b1b1734c397d063bd5
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 12:03:15 +0200

    Ad secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     before_save { sef.email = email.downcase }
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
+    has_secure_password
 end
