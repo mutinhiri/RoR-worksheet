b726f3d8b5b0c43b7bccb31333b366b96a39bc34
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 13:00:40 +0200

    Add changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:success] = 'Success'
+            redirect_to articles_path
 
         else
             render 'new'
