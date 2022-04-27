953a11f7c1e68299e3970a41f60d07c745e8554e
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 08:53:13 +0200

    Add associtions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
