af83753b9e4d617437ca8bbfb7e3f16a80b516ec
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 13:00:05 +0200

    Add changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save 
+        if @user.save
+            flash[:success] = ''
+
+        else
+            render 'new'
     end
 
 
