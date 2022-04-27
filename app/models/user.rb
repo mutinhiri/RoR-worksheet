94aabc0aa77334106dd80d14f2c11c157015429c
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 08:58:26 +0200

    Add database save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :articles
-    before_save { sef.email = email.downcase}
+    before_save { sef.email = email.downcase }
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
