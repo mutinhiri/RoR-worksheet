7b351ddd0c97940257ca87e5ccd28c9822e65400
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 12:59:00 +0200

    Add params send

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,5 @@ class UsersController < ApplicationController
 
     private 
     def user_params
-        params.require(:user).permit(:username)
+        params.require(:user).permit(:username, :email, :password)
 end
