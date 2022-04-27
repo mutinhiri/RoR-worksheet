d1613f0ba0dd781e56bcd4f53d27a97818e9c462
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 08:08:26 +0200

    Remove redundamt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }
-    VALID_EMAIL_REGEX
     validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
