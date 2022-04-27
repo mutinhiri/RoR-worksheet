a34104e692ad9c153164609e74bcc9c4ac8189a2
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-27 08:55:25 +0200

    Add user id validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
     belongs_to :user
     validates :title, presence: true
     validates :description, presence: true
-    validates 
+    validates :user_id, presence: true 
 end
