73ca9d3d626fabf7ba037f6a04f34bb5e4c4ec11
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-29 19:05:15 +0200

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, :except => [:new]
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
-  post 'login'
+  post 'login', to: 'sessions#create'
   # post 'users', to: 'users#create'
 
 end
