aa7223816d66ba183398e0d6bcdc258848e83f58
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-29 18:55:04 +0200

    Route to new file

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, :except => [:new]
   get 'signup', to: 'users#new'
-  get 'login' 
+  get 'login', to: 
   # post 'users', to: 'users#create'
 
 end
