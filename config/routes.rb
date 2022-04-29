a4ce70ca3d3261a9e581a56c5000e313a58092f8
mutinhiri <barnabmutinhiri@gmail.com>
2022-04-29 18:55:31 +0200

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, :except => [:new]
   get 'signup', to: 'users#new'
-  get 'login', to: 
+  get 'login', to: 'sessions#new'
   # post 'users', to: 'users#create'
 
 end
