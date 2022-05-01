ae016e50ec6b665e58cdfa92b9cdcb029206f57b
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:30:53 +0200

    Render new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
+        else
+            render 'new'
     end
 
     def destroy
