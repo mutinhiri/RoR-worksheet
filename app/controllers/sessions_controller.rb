baf938af7403f6875009b3bddf14cb375364836a
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:31:05 +0200

    End the block

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
         else
             render 'new'
+        end
     end
 
     def destroy
