2b0cf435bbbca4efb1285efe645fe511880fb299
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:37:17 +0200

    Add message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
+            flash[:success] = 'You have successfully log in'
         else
             render 'new'
             flash.now[:danger] = 'There was an error'
