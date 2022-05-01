36e9b1c3dc3a8789f40691f0964ccb243ecb4d9a
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:54:33 +0200

    Add changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:success] = 'You have successfully log in'
-            redirect_to users_path(user)
+            redirect_to user_path(user)
         else
             render 'new'
             flash.now[:danger] = 'There was an error'
