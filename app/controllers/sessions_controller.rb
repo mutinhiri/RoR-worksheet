ee2e9ed16c1c13d95b24acee3c7382d81bee0857
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:47:50 +0200

    Add changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        flash[:]
+        flash[:success] = 'you have successfully loged out'
     end
 end 
