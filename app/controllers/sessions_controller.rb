32370a3176b363785987c3cdea764047aa41af7a
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:28:45 +0200

    Check user availability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate
     end
 
     def destroy
