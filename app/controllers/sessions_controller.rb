87145302a20f59117403410368bf5bf52688e6c6
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:40:11 +0200

    Add save user id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-            session[:user_id]
+            session[:user_id] = user.id
             flash[:success] = 'You have successfully log in'
             redirect_to users_path(user)
         else
