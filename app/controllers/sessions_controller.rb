4b77dcdee73dd926c46400b3bfaaf24bf397a20c
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 16:38:20 +0200

    Add specific user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             flash[:success] = 'You have successfully log in'
-            redirect_to 
+            redirect_to users_path(user)
         else
             render 'new'
             flash.now[:danger] = 'There was an error'
