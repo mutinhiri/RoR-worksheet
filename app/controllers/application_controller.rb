a6fe2c93d9968bef8d9668f148b06135cf9b889c
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-01 17:33:19 +0200

    Add changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
     def require_user
         if !logged_in?
             flash[:danger] = "You must be logged in to perfom operation"
+            redirect_to root_path
     end
 end
