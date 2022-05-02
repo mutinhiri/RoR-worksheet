473dece4f3de45324c546378b35022e1090ed031
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-02 20:08:28 +0200

    Add redirect to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = 'Success #{user.username}'
-            redirect_to articles_path
+            redirect_to user_path
 
         else
             render 'new'
