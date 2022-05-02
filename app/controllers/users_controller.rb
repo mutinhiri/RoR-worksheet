06d9ca4ea609d197a300e5b874d602281b62e6fd
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-02 20:08:52 +0200

    Add user param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         if @user.save
             session[:user_id] = @user.id
             flash[:success] = 'Success #{user.username}'
-            redirect_to user_path
+            redirect_to user_path(@user)
 
         else
             render 'new'
