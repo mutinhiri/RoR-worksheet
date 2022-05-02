ab5e01555bb7a293181f76568f66da82b812b8af
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-02 21:30:59 +0200

    Add new params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: []
     def new
         @user = User.new 
     end
