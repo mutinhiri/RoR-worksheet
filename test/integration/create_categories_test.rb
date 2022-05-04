5b82c3c669d63cb4466273e339f831d81acac663
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 22:27:01 +0200

    Assert new template

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,5 +3,6 @@ require "test_helper"
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     test "get new categories form and create category" do
         get new_category_path
+        assert_template "categor"
     end
 end
