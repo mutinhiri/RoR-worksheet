7e7c13950125b599730b0593ec62e75526baa5e1
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 21:54:36 +0200

    Add new test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,4 +3,7 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     test "Should get categories index " do
     end
+
+    test "should get test " do
+    end
 end
