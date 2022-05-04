e6a1984e36a3f7a5662c3b6b20ba42ca5f745ddf
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 15:04:42 +0200

    Add new table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_184626) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_130234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_184626) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
