3ec0420a9298e7be5a8e6fffa09d7b51f729359c
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 22:24:49 +0200

    Add new path

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     test "get new categories form and create category" do
-        get 
+        get new_category_path
     end
 end
