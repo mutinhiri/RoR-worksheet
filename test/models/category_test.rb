8b0c9b8ac5d690761fb9f73235575637701827ab
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 15:14:32 +0200

    If method is valid

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -17,4 +17,5 @@ class CategoryTest < ActiveSupport::TestCase
     test "name should be unique" do
         @category.save
         category2 = Category.new(name: "sports")
+        assert_not category2.valid?
 end
