81e3d9934b8448d58cd81e203f9fa02c17ca3a62
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-04 21:58:29 +0200

    Get success message

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     test "Should get categories index " do
         get categories_path
+        assert_response :success
     end
 
     test "should get test " do
