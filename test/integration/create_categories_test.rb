9e1d440c0322cb1e54039e9543a4f790711c8c48
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 21:15:45 +0200

    Add sign in

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -7,6 +7,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         end
 
     test "get new categories form and create category" do
+        sign_in_as
         get new_category_path
         assert_template "categories/new"
         assert_difference "Category.count" do
