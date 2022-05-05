a4b419c60e93fe706f136a67230992d36e9d516f
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 21:12:35 +0200

    Instantiate user

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,8 +3,9 @@ require "test_helper"
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
     def setup
+        @user =
     end
-    
+
     test "get new categories form and create category" do
         get new_category_path
         assert_template "categories/new"
