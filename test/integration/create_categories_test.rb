1c6311158cb388728242090b4900191629db5d2e
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 17:41:40 +0200

    Add new path

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,7 +5,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         get new_category_path
         assert_template "categories/new"
         assert_difference "Category.count", 1 do
-            post_via redirect
+            post_via redirect categories_path
         end
     end
 end
