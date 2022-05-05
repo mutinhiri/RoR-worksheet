ad7255f4b0dd154d401040e82a6f9db78cf34237
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 21:05:43 +0200

    Add user email

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   def sign_in_as(user, password)
-    post login_path, params: {session: }
+    post login_path, params: {session: {email: user.email}}
 
   # Add more helper methods to be used by all tests here...
 end
