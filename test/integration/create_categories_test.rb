c39378cb4321623b36a50ae8b662967da958d9d3
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 17:40:16 +0200

    Create new ctegory

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -4,5 +4,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     test "get new categories form and create category" do
         get new_category_path
         assert_template "categories/new"
+        assert_difference "Category.count", 1
     end
 end
