860b916baf3b11c09af061374132f1e6809a38f7
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:51:54 +0200

    Add name category

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     def setup
         @category = Category.create(name: "sports")
-        @category2 = 
+        @category2 = Category.create
     end
 end
