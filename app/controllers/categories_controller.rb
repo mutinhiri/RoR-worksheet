e3b9f69c35ada3fcac6064ded056f51fe00c9328
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:18:51 +0200

    New category save

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,7 @@ class CategoriesController < ApplicationController
     def create
         @category = Category.new(category_params)
         if @category.save
+            flash[:success] = "category was created successfully"
 
         else
             render 'new'
