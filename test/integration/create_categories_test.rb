d182c5f31ee9762639e4611c2c016cba9c1a77b8
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:38:24 +0200

    No diff

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -4,7 +4,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     test "get new categories form and create category" do
         get new_category_path
         assert_template "categories/new"
-        assert_difference "Category.count", 1 do
+        assert_difference "Category.count" do
             post categories_path, params: { category: {name: "sports"}}
             follow_redirect
         end
