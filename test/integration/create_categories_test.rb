d4dab84219ffbb5680e8d93741fa7b8c7d3b1cf1
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 17:41:11 +0200

    add new post action

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,6 +5,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         get new_category_path
         assert_template "categories/new"
         assert_difference "Category.count", 1 do
+            post_via redirect
         end
     end
 end
