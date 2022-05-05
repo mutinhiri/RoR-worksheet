05e5b1796b852f22bd944f401469d6a7db7d42b2
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:55:56 +0200

    Add template assert

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -7,6 +7,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
         test "Should show categories listing" do
             get categories_path
+            assert_template
         end 
     end
 end
