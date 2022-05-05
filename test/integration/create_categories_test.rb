5a3b742f80e9839694df4be898fc2b5297ce9ce3
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:38:05 +0200

    Assert no diff

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -13,5 +13,13 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
 
     test "Invalid category submission results in failure" do
+        get new_category_path
+        assert_template "categories/new"
+        assert_no_difference "Category.count", 1 do
+            post categories_path, params: { category: {name: "sports"}}
+            follow_redirect
+        end
+        assert_template "categories/index"
+        assert_match "sports", response.body
     end
 end
