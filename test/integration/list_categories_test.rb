6e915803195f15a3486b4345a907218da938ddf4
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:50:41 +0200

    Add category listing

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     def setup
-        @category = Category.create()
+        @category = Category.create(name: "sports")
     end
 end
