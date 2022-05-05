a3d7661777a84a726d44eeb333225a00331761d6
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 17:45:49 +0200

    Add new category name

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,7 +5,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         get new_category_path
         assert_template "categories/new"
         assert_difference "Category.count", 1 do
-            post categories_path
+            post categories_path, params: { category: {name: "sports"}}
         end
     end
 end
