4952a861168b3e516c5dc359c1ebcde9b92d516b
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 18:41:56 +0200

    Panel body render

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -20,5 +20,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         end
         assert_template "categories/new"
         assert_select "h2.panel-title"
+        assert_select "div.panel-body"
     end
 end
