9e415b617aec954dfaf33a3d4b1fbeb866f80a3d
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-05 20:53:57 +0200

    Current user not admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
     end
 
     def require_admin
-        if !logged_in? || (logged_in? and !current_user)
+        if !logged_in? || (logged_in? and !current_user.admin)
             flash[:danger] = "Onky admins can perfom that action"
             redirect_to categories_path
         end
