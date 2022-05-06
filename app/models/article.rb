209472f10a79e2b23da793e07723093d9a608e05
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-06 12:18:51 +0200

    Add categories listing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
     has_many :article_categories
+    has_many :categories
     validates :title, presence: true
     validates :description, presence: true
     validates :user_id, presence: true 
