81d2247d42acdd7894566be0dacf9797d58e9301
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-06 12:19:26 +0200

    Dependent article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
     has_many :article_categories
-    has_many :categories
+    has_many :categories, through: :article
     validates :title, presence: true
     validates :description, presence: true
     validates :user_id, presence: true 
