64b6c70d3d9301a6c1813c2104c37853622d62fd
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-06 12:11:23 +0200

    Add integer column

--- db/migrate/20220506100921_create_article_categories.rb
+++ db/migrate/20220506100921_create_article_categories.rb
@@ -1,7 +1,7 @@
 class CreateArticleCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :article_categories do |t|
-      t.integer
+      t.integer :article_id
 
       t.timestamps
     end
