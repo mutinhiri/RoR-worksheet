5c260f44d964ba23ef8e526cebfc99e9a6f01e69
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-06 12:18:27 +0200

    has many plural

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_categories
     validates :title, presence: true
     validates :description, presence: true
     validates :user_id, presence: true 
