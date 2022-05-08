9f04ce63cf981a858a3c893e53938813b7716a59
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-08 17:56:00 +0200

    New category path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,7 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         if @category.update(category_params)
             flash[:success] = "name was successfuly updated"
-            redirect_to category_path
+            redirect_to category_path(@category)
 
         else 
             render 'edit'
